efd14df9dc29b4ecc5bb6c27e4213e8ef9c41f23
Zeeshan Ashraf <zashraf080@gmail.com>
2024-07-04 11:22:48 +0500

    channel ids updated

--- app/src/main/java/com/example/youtubechanelproject/VideoViewModel.kt
+++ app/src/main/java/com/example/youtubechanelproject/VideoViewModel.kt
@@ -8,7 +8,28 @@ import androidx.lifecycle.LiveData
 class VideoViewModel(application: Application) : AndroidViewModel(application) {
     private var videoRepository: VideoRepository? = null
     var videosLiveData: LiveData<List<Video>>? = null
-    private val channelIds = listOf("UCapJpINJKHzflWwCQ8Kse2g", "UCupvZG-5ko_eiXAupbDfxWw")
+    //private val channelIds = listOf("UCapJpINJKHzflWwCQ8Kse2g", "UCupvZG-5ko_eiXAupbDfxWw")
+    private val channelIds = listOf( "UCaszgR2TH3qNw_CxLHAd2SQ",
+        "UCazLt7y_UefRliODaUQznxg",
+        "UCzMWRU-8D1IDafX2lA8d5xA",
+        "UCxO4FDEsoD7geDkoIuOaCyw",
+        "UCY7eFg5TEMsk_aTqM0pVB4w",
+        "UCnkYymEbl1qZ0VhGrzH9guw",
+        "UCeqdeoG3nRSkkbBKT9u3wQQ",
+        "UCxaMbYDd4o_zVvfr9_wKAxQ",
+        "UC620CWJ3QI2NjKFKhwKU2aw",
+        "UCpkGqplkgxbHtzwHFPzzvUA",
+        "UCAsvFcpUQegneSh0QAUd64A",
+        "UCKD6btkq3JYZc7a6XpssMkA",
+        "UCUszZwlbtq7R4ltY5pTRDww",
+        "UC-gVcoup_u1yeGkqav-Lqkw",
+        "UCCWpMUHRxsokns1bwrWvGCg",
+        "UCFqEO3bSGcp5I8HVgXYdYRQ",
+        "UCqGdM1DKE7iW2wH21QmwHoQ",
+        "UC8jWGeUHBDg6Vbq1rICVwBA",
+        "UCGhb5Gy-TAp0XWV26za_mcA",
+        "UC1yuv89ftFyDQ8Ibre7cRgg",
+        "UCVmuknBRlZvxDLJqnDfP7Ag")
 
     init {
         /* val youTubeApi = RetrofitClient.getClient("https://www.googleapis.com/youtube/v3/")
@@ -30,7 +51,7 @@ class VideoViewModel(application: Application) : AndroidViewModel(application) {
         videoRepository = VideoRepository(youTubeApi)
         videosLiveData = videoRepository!!.getLatestVideosFromChannels(
             channelIds,
-            3,
+            2,
             MainActivity.Google_Cloud_API_KEY)
     }
 }
